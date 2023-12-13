6c5eba28d3bb04b687e9adead1d7360e5ff88500
abaurens <abaurens@student.42.fr>
2023-12-13 03:07:39 +0100

    Update Windows TemplateScript

--- Setup.bat
+++ Setup.bat
@@ -1,3 +1,11 @@
+@if exist .\TemplateScripts\RenameTemplate.ps1 (
+	@Powershell.exe -executionpolicy remotesigned -File .\TemplateScripts\RenameTemplate.ps1
+	@if exist .\TemplateScripts\Marker (
+	  @RD /S /Q ".\TemplateScripts"
+	)
+)
+
+
 call premake\Windows\premake5.exe vs2022 || (
 	pause
 )
